99fb9b52648205580525f6f70227298986f39183
Komal Nadeem <koomalkhan111@gmail.com>
2024-10-27 21:14:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let age=alert("I am 17 Years Old");
+let visit=alert(" You have  Visited this site 14 times");
+
+document.write("My birth year is 1990","<br>")
+document.write("Data type of my declared variable is number.") 
+
